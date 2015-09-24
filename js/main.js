98717006e7d6817e5fb6bfe937efe7b40335200d
Mattias Robertsson <mattias.robertsson88@gmail.com>
2015-09-24 20:07:31 +0200

    New styles
    Removal of images etc

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
+if ($(window).width() < 361) {
+        $(function() {
+        	$('body').vegas({
+                slides: [
+                    { src: 'img/socialwreck.jpg' },
+                      	]
+                    });
+                });
+            } else {
+        	$(function() {
+                $('body').vegas({
+                  slides: [
+                    { src: 'img/jojo2.jpg' },
+                  ]
+                });
+            });
+            }
 
+$(".animsition").animsition({
+        inClass: 'fade-in-down-lg',
+        outClass: 'fade-out-down-lg',
+        inDuration: 1000,
+        outDuration: 500
+});
--- test.php
+++
@@ -1,1 +0,0 @@
-<h1><?php echo Shirts 4 Mike; ?></h1>
